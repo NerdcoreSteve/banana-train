2e373a2fb90c6272be4bc3e10b71b71ff48e2040
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-08-27 11:57:35 -0600

    it is now a react app

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,2 +1,15 @@
-const w = () => console.log('Hey batman!')
-w()
+  document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
+  ':35729/livereload.js?snipver=1"></' + 'script>')
+
+const
+    R = require('ramda'),
+    React = require('react'),
+    ReactDOM = require('react-dom')
+
+ReactDOM.render(
+    <div>
+        <h1>Banana Train</h1>
+        <p>A productivity tool for banana potatoes</p>
+    </div>,
+    document.getElementById('banana-train')
+)
