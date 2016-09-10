c18ea9ef221401489df7a41c4be3815740819e5d
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-09-10 12:29:00 -0600

    removed socket.io

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ const
     express = require('express'),
     app = express(),
     http = require('http').createServer(app)
-    io = require('socket.io')(http)
 
 app.use(express.static(__dirname + '/public'))  
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
-
-io.on('connection', socket => {
-    io.emit('refresh page')
-})
 
 http.listen(3000, () => console.log('Example app listening on port 3000!'))
