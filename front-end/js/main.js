fc2f2cee517adaf69b0e7d26e02227afea16f4f2
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-09-10 13:10:00 -0600

    removed socket io stuff

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,14 +1,7 @@
-//TODO you're not doing this right,
-// follow http://socket.io/get-started/chat/
-// more closely
-// then figure out how to send a signal to the page
 const
     R = require('ramda'),
     React = require('react'),
-    ReactDOM = require('react-dom'),
-    socket = io()
-
-socket.on('refresh page', () => console.log('reloading'))
+    ReactDOM = require('react-dom')
 
 ReactDOM.render(
     <div>
